eda719adbc19fa2d97df4479ff48d458a7b5fc1d
Julio <julioaranabr@yahoo.com.br>
2022-04-27 20:36:17 -0300

    created model product

---
+++ back-end/src/database/models/Product.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define('Product', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+    urlImage: DataTypes.STRING,
+  },
+  {
+    timestamps: false, 
+    tableName: 'Products',
+    underscored: true,
+  });
+
+  return Product;
+};
