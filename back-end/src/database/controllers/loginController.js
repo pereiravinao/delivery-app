e2dd898be80892bce682422713dfc88d09e81261
Julio <julioaranabr@yahoo.com.br>
2022-04-28 18:41:13 -0300

    refact app

--- back-end/src/database/controllers/loginController.js
+++ back-end/src/database/controllers/loginController.js
@@ -1,13 +1,15 @@
-/* const { getLogin } = require('../services/loginService');
+const { getLogin } = require("../services/loginService");
+
 
 const login = async (req, res) => {
   try {
     const login = req.body;
     const data = await getLogin(login);
-    return res.status(201).json({ data });
+    if(!data) return res.status(404).json('Not found');
+    return res.status(201).json(data);
   } catch (err) {
-    return res.status(500).json({ message: 'Erro interno', error: err.message });
+    return res.status(500).json(err);
   }
 }
 
-module.exports = { login }; */
+module.exports = { login };
