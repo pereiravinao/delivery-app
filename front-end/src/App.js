a7ebb1af7881e281cb5a91c8e76bd7c3bffb16d1
Israel Nazareth <israel.fab@hotmail.com>
2022-05-02 17:39:17 -0300

    feat: implementa inputs e testsids de elementos em Login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AppRoutes from './routes/AppRoutes';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import DeliveryProvider from './context/deliveryProvider';
 
 function App() {
--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import '../style/Login.css';
 
 const Login = () => (
-  <>
-    <h1>Login</h1>
-    <h2>Teste</h2>
-  </>
+  <div>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Login</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" data-testid="common_login__input-email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Senha</Form.Label>
+        <Form.Control type="password" placeholder="Password" data-testid="common_login__input-password" />
+      </Form.Group>
+      <Button variant="primary" type="submit" data-testid="common_login__button-login">
+        Login
+      </Button>
+      <Button variant="secondary" type="submit" data-testid="common_login__button-register">
+        Ainda não tenho conta
+      </Button>
+    </Form>
+    <p className="error" data-testid="common_login__element-invalid-email">
+      Mensagem de erro.
+    </p>
+  </div >
 );
 
 export default Login;
