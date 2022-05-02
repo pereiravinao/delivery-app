0a8a190928ab5e20cd0d9da63fb0c2dc0c943579
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-05-02 18:31:21 -0400

    feat: adicionar estado no Login

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,31 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 import '../style/Login.css';
 
-const Login = () => (
-  <div>
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Login</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" data-testid="common_login__input-email" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Senha</Form.Label>
-        <Form.Control type="password" placeholder="Password" data-testid="common_login__input-password" />
-      </Form.Group>
-      <Button variant="primary" type="submit" data-testid="common_login__button-login">
-        Login
-      </Button>
-      <Button variant="outline-primary" type="submit" data-testid="common_login__button-register">
-        Ainda não tenho conta
-      </Button>
-    </Form>
-    <Alert key='danger' variant='danger' className="error" data-testid="common_login__element-invalid-email">
-      Mensagem de erro.
-    </Alert>
-  </div >
-);
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const isEmailValid = (userEmail) => {
+    const regexEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return regexEmail.test(userEmail);
+  };
+
+  const MIN_LENGTH = 6;
+
+  return (
+    <div>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Login</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" data-testid="common_login__input-email" onChange={ ({ target }) => setEmail(target.value) } />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Senha</Form.Label>
+          <Form.Control type="password" placeholder="Password" data-testid="common_login__input-password" onChange={ ({ target }) => setPassword(target.value) } />
+        </Form.Group>
+        <Button variant="primary" type="submit" data-testid="common_login__button-login" disabled={ !(isEmailValid(email) && password.length >= MIN_LENGTH) }>
+          Login
+        </Button>
+        <Button variant="outline-primary" type="submit" data-testid="common_login__button-register">
+          Ainda não tenho conta
+        </Button>
+      </Form>
+      <Alert key='danger' variant='danger' className="error" data-testid="common_login__element-invalid-email">
+        Mensagem de erro.
+      </Alert>
+    </div >
+  );
+};
 
 export default Login;
--- front-end/src/routes/AppRoutes.js
+++ front-end/src/routes/AppRoutes.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate, Navigate } from 'react-router-dom';
 import Login from '../pages/Login';
 
-const AppRoutes = () => (
-  <Routes>
-    <Route exact path="/" element={ <Login /> } />
-    <Route exact path="/login" element={ <Login /> } />
-  </Routes>
-);
+const AppRoutes = () => {
+  const navigate = useNavigate();
+  
+  return(
+    <Routes>
+      <Route path="/" element={ <Login /> }>
+        <Navigate to="/login" replace={true} />
+      </Route>
+      <Route exact path="/login" element={ <Login /> } />
+    </Routes>
+  );
+};
 
 export default AppRoutes;
